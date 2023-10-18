74d3f8c90d877ef32c03eafde5ebaa1cf4594105
samlrun <85369403+samlrun@users.noreply.github.com>
2023-10-18 17:38:06 -0600

    initial steps of traveltimes.R

--- R/traveltimes.R
+++ R/traveltimes.R
@@ -1,3 +1,11 @@
 format_traveltimes <- function(traveltimes) {
-  
+  # simplify data
+  df <- traveltimes %>%
+    filter(`$VEHICLETRAVELTIMEMEASUREMENTEVALUATION:SIMRUN` == "AVG") %>%
+    select(
+      movement = `VEHICLETRAVELTIMEMEASUREMENT\\NAME`,
+      vehicles = `VEHS(ALL)`,
+      avg_travel_time_sec = `TRAVTM(ALL)`,
+      avg_distance_traveled_ft = `DISTTRAV(ALL)`
+    )
 }
