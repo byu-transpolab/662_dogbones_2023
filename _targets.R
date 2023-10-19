2aa4d61f865ae94329c373165f2c2bc075fa43c5
samlrun <85369403+samlrun@users.noreply.github.com>
2023-10-18 18:44:44 -0600

    changed line skip for existing travel times

--- _targets.R
+++ _targets.R
@@ -3,7 +3,6 @@
 library(targets)
 library(tarchetypes)
 library(lubridate)
-# library(tidyverse)
 
 tar_option_set(
   packages = c("tidyverse", "DiagrammeR", "gtools", "readxl"),
@@ -82,11 +81,11 @@ memo_targets <- tar_plan(
   ex_pm_los_formatted = format_los(ex_pm_los, intersection_translation),
   
   tar_target(ex_am_traveltimes_file, "vissim/existing_2023_AM/existing_AM_Vehicle Travel Time Results.att", format = "file"),
-  ex_am_traveltimes = read_att(ex_am_traveltimes_file, lineskip = 20),
+  ex_am_traveltimes = read_att(ex_am_traveltimes_file, lineskip = 19),
   ex_am_traveltimes_formatted = format_traveltimes(ex_am_traveltimes),
   
   tar_target(ex_pm_traveltimes_file, "vissim/existing_2023_PM/existing_PM_Vehicle Travel Time Results.att", format = "file"),
-  ex_pm_traveltimes = read_att(ex_pm_traveltimes_file, lineskip = 20),
+  ex_pm_traveltimes = read_att(ex_pm_traveltimes_file, lineskip = 19),
   ex_pm_traveltimes_formatted = format_traveltimes(ex_pm_traveltimes),
   
   #### Memo 2: New Interchange ####
